3898ee0e50fe17a0c5aae3b83fb4d3e6879240c3
Devansh-Aage <devanshaage@gmail.com>
2024-09-01 18:08:49 +0530

    first commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Include all JavaScript and JSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
