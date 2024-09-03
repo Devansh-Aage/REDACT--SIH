d79d03436606e90863ccdddee97266cbcf573748
Nirav Patel <nirav.p@somaiya.edu>
2024-09-03 17:54:40 +0530

    check it out login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
 import LandingPage from './component/LandingPage';
+import Home from './component/Home'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
